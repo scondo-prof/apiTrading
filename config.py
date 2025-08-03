2a86efa3b889ee9fd907a07784264c441a7ee077
Scooter Condo <scondo.prof@gmail.com>
2025-08-03 18:38:30 -0400

    dev/setup start to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+headers: dict[str, str] = {
+    "APCA-API-KEY-ID": os.getenv("APCA_API_KEY_ID"),
+    "APCA-API-SECRET-KEY": os.getenv("APCA_API_SECRET_KEY"),
+}
+
+paper_trading_base_url: str = "https://paper-api.alpaca.markets/v2"
