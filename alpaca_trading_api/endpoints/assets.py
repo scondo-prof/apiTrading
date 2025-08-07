a397de1584e1f7cd309fa43f6a5e26a3caf01514
Scott Condo <scottcondo@Scotts-MacBook-Pro.local>
2025-08-06 22:42:41 -0400

    dev/started get_options_contracts function, started adding default values to query string parameters had explicit default values in the API Docs

--- alpaca_trading_api/endpoints/assets.py
+++ alpaca_trading_api/endpoints/assets.py
@@ -11,7 +11,11 @@ from config import headers, paper_trading_base_url, trading_base_url
 
 
 def get_assets(
-    status: str, asset_class: str, exchange: str, attributes: list[str], paper_trading: bool
+    status: str = "Active",
+    asset_class: str = "us_equity",
+    exchange: str = "NYSE",
+    attributes: list[str] = ["ptp_no_exception", "ptp_with_exception", "ipo", "has_options", "options_late_close"],
+    paper_trading: bool = True,
 ) -> list[dict[str, any]]:
     if paper_trading:
         url: str = (
@@ -27,7 +31,7 @@ def get_assets(
     return response_json
 
 
-def get_asset_by_id_or_symbol(symbol_or_asset_id: str, paper_trading: bool) -> dict[str, any]:
+def get_asset_by_id_or_symbol(symbol_or_asset_id: str, paper_trading: bool = True) -> dict[str, any]:
     if paper_trading:
         url: str = f"{paper_trading_base_url}/assets/{symbol_or_asset_id}"
     else:
@@ -38,8 +42,36 @@ def get_asset_by_id_or_symbol(symbol_or_asset_id: str, paper_trading: bool) -> d
     return response_json
 
 
-def get_option_contracts():
-    pass
+def get_option_contracts(
+    underlying_symbols: str,
+    root_symbol: str,
+    paper_trading: bool = True,
+    show_deliverables: bool = True,
+    status: str = "active",
+    expiration_date: str = None,
+    expiration_date_gte: str = None,
+    expiration_date_lte: str = None,
+    type: str = "call",
+    style: str = "american",
+    strike_price_gte: int = None,
+    strike_price_lte: int = None,
+    page_token: str = None,
+    limit: int = 100,
+    ppind: bool = None,
+) -> dict[str, any]:
+    if paper_trading:
+        url: str = f"{paper_trading_base_url}/options/contracts?underlying_symbols={underlying_symbols};root_symbol={root_symbol};paper_trading={paper_trading};show_deliverables={show_deliverables};status={status};type={type};style={style};limit={limit};"
+
+    else:
+        url: str = f"{trading_base_url}/options/contracts?underlying_symbols={underlying_symbols};root_symbol={root_symbol};paper_trading={paper_trading};show_deliverables={show_deliverables};status={status};type={type};style={style};limit={limit};"
+
+    request_json = httpx.request(method="GET", url=url, headers=headers).json()
+
+    with open("test.json", "w") as test_file:
+        test_file.write(json.dumps(request_json))
+
+    test_file.close()
+    return request_json
 
 
 def get_option_contract_by_id_or_symbol():
@@ -52,13 +84,7 @@ def get_us_treasuries():
 
 if __name__ == "__main__":
     # print(
-    #     get_assets(
-    #         status="active",
-    #         asset_class="us_equity",
-    #         exchange="NYSE",
-    #         attributes=["ptp_no_exception", "ptp_with_exception", "ipo", "has_options", "options_late_close"],
-    #         paper_trading=True,
-    #     )
+    #     get_assets()
     # )
 
-    print(get_asset_by_id_or_symbol(symbol_or_asset_id="BCH/USD", paper_trading=True))
+    # print(get_asset_by_id_or_symbol(symbol_or_asset_id="BCH/USD", paper_trading=True))
